9cdbb5043044fb3856ae3d09f507b776a1b82791
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2024-08-22 20:10:06 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://nelsonluengas.github.io/consumo_apis'
+  base: 'https://NELSONLUENGAS.github.io/consumo_apis'
 })
